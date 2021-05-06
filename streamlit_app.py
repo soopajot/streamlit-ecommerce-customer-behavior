6f1d8a622eeeeb7d84fbe8724cf3aad3c6f0ca2c
soopajot <soopajot@gmail.com>
2021-05-06 16:57:29 +0200

    test images

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,9 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+from PIL import Image
+image = Image.open('images/streamlit.jpg')
+st.image(image, caption='Notre Heatmap')
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
