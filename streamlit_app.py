3fb8a1cbd761f91bc76183846ed8b941285bb233
soopajot <soopajot@gmail.com>
2021-05-06 17:00:00 +0200

    upload images

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,35 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+st.header('eCommerce Customer Behavior')
+
 from PIL import Image
 image = Image.open('images/streamlit.jpg')
 st.image(image, caption='Notre Heatmap')
+
+image1 = Image.open('images/img-1.png')
+st.image(image1, caption='Notre img')
+
+image2 = Image.open('images/img-2.png')
+st.image(image2, caption='Notre img')
+
+image3 = Image.open('images/img-3.png')
+st.image(image3, caption='Notre img')
+
+image4 = Image.open('images/img-4.png')
+st.image(image4, caption='Notre img')
+
+image5 = Image.open('images/img-5.png')
+st.image(image5, caption='Notre img')
+
+image6 = Image.open('images/img-6.png')
+st.image(image6, caption='Notre img')
+
+image7 = Image.open('images/img-7.png')
+st.image(image7, caption='Notre img')
+
+image8 = Image.open('images/img-8.png')
+st.image(image8, caption='Notre img')
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
